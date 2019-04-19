c478ced4687061705279e503909c82f0390d2816
niujt <670482466@qq.com>
2019-04-19 13:26:38 +0800

    用Stream和forEach代替传统for循环

--- src/main/java/WordUtils.java
+++ src/main/java/WordUtils.java
@@ -1,29 +1,28 @@
 import demo.Leg;
 import demo.LegUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.*;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.*;
 
 public class WordUtils {
-
-    private static final String PATH = "D:\\";
     public static void main(String[] args) throws IOException {
-        String filepathString = PATH + "场外衍生品结算通知书_190220.docx";
-        String destpathString = PATH + "/2ttt.docx";
+        String filepathString ="D:\\场外衍生品结算通知书_190220.docx";
+        String destpathString ="D:\\2ttt.docx";
         try (FileInputStream fileInputStream = new FileInputStream(filepathString);
-             OutputStream os = new FileOutputStream(destpathString);
+             OutputStream os = new FileOutputStream(destpathString)
         ) {
             XWPFDocument xwpfDocument = new XWPFDocument(fileInputStream);
             replaceDocument(xwpfDocument, "中正有限公司");
             xwpfDocument.write(os);
         }
-
     }
 
 
-    private static void replaceDocument(XWPFDocument document,String partyName) {
+    private static void replaceDocument(XWPFDocument document, String partyName) {
         Iterator<XWPFTable> tablesIterator = document.getTablesIterator();
         while (tablesIterator.hasNext()) {
             XWPFTable table = tablesIterator.next();
@@ -37,105 +36,64 @@ public class WordUtils {
         XWPFTableRow targetRow = table.insertNewTableRow(rowIndex);
         //复制行属性
         targetRow.getCtRow().setTrPr(sourceRow.getCtRow().getTrPr());
-        List<XWPFTableCell> cellList = sourceRow.getTableCells();
-        if (null == cellList) {
-            return targetRow;
-        }
-        //复制列及其属性和内容
-        XWPFTableCell targetCell = null;
-        for (XWPFTableCell sourceCell : cellList) {
-            targetCell = targetRow.addNewTableCell();
+        sourceRow.getTableCells().stream().filter(sourceCell->sourceCell!=null).forEach(sourceCell-> {
+            XWPFTableCell targetCell = targetRow.addNewTableCell();
             //列属性
             targetCell.getCTTc().setTcPr(sourceCell.getCTTc().getTcPr());
-            //段落属性
-            if (sourceCell.getParagraphs() != null && sourceCell.getParagraphs().size() > 0) {
-                targetCell.getParagraphs().get(0).getCTP().setPPr(sourceCell.getParagraphs().get(0).getCTP().getPPr());
-                if (sourceCell.getParagraphs().get(0).getRuns() != null && sourceCell.getParagraphs().get(0).getRuns().size() > 0) {
-                    XWPFRun cellR = targetCell.getParagraphs().get(0).createRun();
-                    //cellR.setText(sourceCell.getText());
-                    cellR.setBold(sourceCell.getParagraphs().get(0).getRuns().get(0).isBold());
-                } else {
-                    //targetCell.setText(sourceCell.getText());
-                }
-            } else {
-                //targetCell.setText(sourceCell.getText());
+            targetCell.getParagraphs().get(0).getCTP().setPPr(sourceCell.getParagraphs().get(0).getCTP().getPPr());
+            if (sourceCell.getParagraphs().get(0).getRuns() != null && sourceCell.getParagraphs().get(0).getRuns().size() > 0) {
+                XWPFRun cellR = targetCell.getParagraphs().get(0).createRun();
+                cellR.setBold(sourceCell.getParagraphs().get(0).getRuns().get(0).isBold());
             }
-        }
-
+        });
         return targetRow;
     }
 
-    private static Map<Integer, List<XWPFTableCell>> getCells(XWPFTable table) {
-        Map<Integer, List<XWPFTableCell>> map = new LinkedHashMap<>();
-        int count = table.getNumberOfRows();//获得表格总行数
-        for (int i = 0; i < count; i++) { //遍历表格的每一行
-            XWPFTableRow row = table.getRow(i);//获得表格的行
-            List<XWPFTableCell> cells = row.getTableCells();//在行元素中，获得表格的单元格
-            map.put(i, cells);
-        }
-        return map;
+    private static int get$Line(XWPFTable table) {
+        return table.getRows().stream()
+                .filter(xwpfTableRow -> xwpfTableRow.getTableCells().stream()
+                        .anyMatch(cell -> cell.getText().contains("${leg.id}")))
+                .findAny()
+                .map(v -> table.getRows().indexOf(v))
+                .orElseThrow(() -> new RuntimeException("can not find the line"));
     }
 
-    private static void replaceTable(XWPFTable table,String partyName) {
-        int count = table.getNumberOfRows();//获得表格总行数
-        int line = 0;
-        int $_line = 0;
-        for (int i = 0; i < count; i++) { //遍历表格的每一行
-            List<XWPFTableCell> cells = getCells(table).get(i);
-            for (XWPFTableCell cell : cells) {   //遍历单元格
-                String content = cell.getText();
-                if (content.contains("${header}")) {
-                    line = i;
-                }
-                if (content.contains("${leg.id}")) {
-                    $_line = i;
-                }
-            }
-        }
-        List<Leg> lesg = new ArrayList<>();
-        lesg = LegUtils.getlegList(lesg);
-        for (Leg leg : lesg) {
-            XWPFTableRow originRow = table.getRow($_line);
-            XWPFTableRow row1 = copy(table, originRow, $_line + 1);
+    private static void replaceTable(XWPFTable table, String partyName) {
+        int $_line = get$Line(table);
+        table.getRows().forEach(
+                xwpfTableRow ->
+                        xwpfTableRow.getTableCells().forEach(cell -> {
+                            String content = cell.getText();
+                            if (content.contains("${partyName}")) {
+                                String cont = content.replace("${partyName}", partyName);
+                                cell.removeParagraph(0);
+                                cell.setText(cont);
+                            }
+                        })
+        );
+        List<Leg> legs = LegUtils.getlegList(new ArrayList<>());
+        replase$2Value(legs,table,$_line);
+
+
+    }
+
+    private static void replase$2Value(List<Leg> legs,XWPFTable table,int $_line){
+        final int $_line_final = $_line;
+        legs.forEach(leg -> {
+            XWPFTableRow originRow = table.getRow($_line_final);
+            XWPFTableRow row1 = copy(table, originRow, $_line_final + 1);
             Map map2 = LegUtils.getListByParamter(leg);
-            for (int i = 0; i < originRow.getTableCells().size(); i++) {
-                XWPFTableCell originCell = originRow.getCell(i);
-                XWPFTableCell newCell = row1.getCell(i);
-                String context = originCell.getText();
-                Object value = map2.get(context);
-                if (value == null) {
-                    continue;
-                }
-                newCell.setText(value.toString());
-                //cell.removeParagraph(0);
-            }
-        }
+            originRow.getTableCells().stream()
+                    .filter(originCell -> map2.get(originCell.getText()) != null)
+                    .forEach(originCell -> {
+                        int i = originRow.getTableCells().indexOf(originCell);
+                        XWPFTableCell newCell = row1.getCell(i);
+                        String context = originCell.getText();
+                        Object value = map2.get(context);
+                        newCell.setText(value.toString());
+                    });
+        });
         table.removeRow($_line);
-        Map map3 = LegUtils.getListByParamter(new Leg());
-        for (int i = 0; i < count - 1; i++) { //遍历表格的每一行
-            List<XWPFTableCell> cells = getCells(table).get(i);
-            for (XWPFTableCell cell : cells) {
-                String content = cell.getText();
-//                if (content.contains("${header}")) {
-//                    // System.out.println(content + "=====================第" + i + "行");
-//                    String cont = content.replace("${header}", "");
-//                    cell.setText(cont);
-//                } else
-                if (content.contains("${partyName}")) {
-                    String cont = content.replace("${partyName}",partyName );
-                    cell.removeParagraph(0);
-                    cell.setText(cont);
-                }
-//                for (Object key : map3.keySet()) {
-//                        if(content.contains((String)key)){
-//                            System.out.println(content);
-////                            content=content.replace((String)key,(String)map3.get(key));
-//                      // cell.removeParagraph(0);//所以这里就要求每一个单元格只能有唯一的变量。
-//                       cell.setText(content);
-//                   }
-//            }
-            }
-        }
     }
 
 }
--- src/main/java/demo/LegUtils.java
+++ src/main/java/demo/LegUtils.java
@@ -10,40 +10,39 @@ import java.util.List;
 import java.util.Map;
 
 public class LegUtils {
-    public static Map getListByParamter(Leg leg){
-            Map map=new LinkedHashMap();
-            try{
-                Class legclazz=leg.getClass();
-                Field[] fields=legclazz.getDeclaredFields();
-                for(Field field:fields){
-                    field.setAccessible(true);
-                    Method method=legclazz.getDeclaredMethod("get"+up2First(field.getName()));
-                    Object o=method.invoke(leg);
-                    map.put("${leg."+field.getName()+"}",o);
-                }
+    public static Map<String, Object> getListByParamter(Leg leg) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        try {
+            Class legclazz = leg.getClass();
+            Field[] fields = legclazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                Method method = legclazz.getDeclaredMethod("get" + up2First(field.getName()));
+                Object o = method.invoke(leg);
+                map.put("${leg." + field.getName() + "}", o);
             }
-            catch (Exception e){
-                e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return map;
     }
 
-    public static List<Leg> getlegList(List<Leg> legs){
-        for(int i=0;i<20;i++){
-            Leg leg=new Leg();
-            leg.setDirection("康贝嗯"+i);
-            leg.setExpirationDate(LocalDate.of(2018,12,3));
+    public static List<Leg> getlegList(List<Leg> legs) {
+        for (int i = 0; i < 20; i++) {
+            Leg leg = new Leg();
+            leg.setDirection("康贝嗯" + i);
+            leg.setExpirationDate(LocalDate.of(2018, 12, 3));
             leg.setHistoryValue(new BigDecimal(12));
             leg.setId(1);
             leg.setInitialValue(new BigDecimal(12));
-            leg.setInstrumentName("asdasda"+i);
+            leg.setInstrumentName("asdasda" + i);
             leg.setOptionType("看跌");
             leg.setProductType("欧式");
             leg.setRemainValue(new BigDecimal(300));
             leg.setSettleAmount(new BigDecimal(12));
-            leg.setSettlementDate(LocalDate.of(2018,2,2));
+            leg.setSettlementDate(LocalDate.of(2018, 2, 2));
             leg.setStrike(new BigDecimal(12));
-            leg.setTradeId("21212"+i);
+            leg.setTradeId("21212" + i);
             leg.setUnderlyerInstrumentId("600572.SH");
             leg.setUnderlyerPrice(new BigDecimal(12));
             legs.add(leg);
@@ -51,15 +50,16 @@ public class LegUtils {
         return legs;
     }
 
-    public static String up2First(String parameter){
-            char[] chars=parameter.toCharArray();
-            chars[0]-=32;
-            return String.valueOf(chars);
-    }
-    public static String low2First(String parameter){
-        char[] chars=parameter.toCharArray();
-        chars[0]+=32;
+    public static String up2First(String parameter) {
+        char[] chars = parameter.toCharArray();
+        chars[0] -= 32;
         return String.valueOf(chars);
     }
 
- }
+    public static String low2First(String parameter) {
+        char[] chars = parameter.toCharArray();
+        chars[0] += 32;
+        return String.valueOf(chars);
+    }
+
+}
